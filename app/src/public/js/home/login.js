5f94c96f42ebafffbc6632d2facad0e767bd68a1
Blowhooo <blowhooo@gmail.com>
2024-04-16 21:12:56 +0900

    로그인을 요청하는 fetch 생성2

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -4,9 +4,6 @@ const id = document.querySelector("#id"),
       password = document.querySelector("#password"),
       loginBtn = document.querySelector("#button");
 
-      console.log(id);
-      console.log("Hello");
-
       loginBtn.addEventListener('click', login)
 
       function login(){
@@ -14,7 +11,6 @@ const id = document.querySelector("#id"),
           id : id.value,
           password : password.value,
         }
-        console.log(req, JSON.stringify(req));
         
         fetch("/login", {
           method: "POST",
