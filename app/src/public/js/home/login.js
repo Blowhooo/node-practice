73ebe2d23717caccb10dbefbfdec9646fee1b254
Blowhooo <blowhooo@gmail.com>
2024-04-16 21:05:16 +0900

    돔을 이용하여 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log('hello');
-console.log('bye');
+const id = document.querySelector("#id"),
+      password = document.querySelector("#password"),
+      loginBtn = document.querySelector("#button");
+
+      console.log(id);
+      console.log("Hello");
+
+      loginBtn.addEventListener('click', login)
+
+      function login(){
+        const req = {
+          id : id.value,
+          password : password.value,
+        }
+      }
