af918a580ac4c8b2fd70f6fc1d7fd003d11c9fb7
Blowhooo <blowhooo@gmail.com>
2024-04-21 21:05:34 +0900

    백엔드 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,44 @@
 "use strict";
 
-const hello = (req,res) => {
-  res.render("index");
+
+
+const output = {
+  home : (req,res) => {
+    res.render("index");
+  },
+  login : (req,res) => {
+    res.render("login");
+  },
 };
 
-const login = (req,res) => {
-  res.render("login");
+const users = {
+  id : ["test", "test2", "윤준후"],
+  password : ["1234", "1234", "123456"]
+};
+
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+          password = req.body.password;
+
+    if(users.id.includes(id)){
+      const idx = users.id.indexOf(id);
+      if(users.password[idx] === password){
+        return res.json({
+          success:true,
+        });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하였습니다.",
+    })
+  }
 };
 
 module.exports = {
-  hello,
-  login
+  output,
+  process
 }
